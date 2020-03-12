670979db095d38576e941fe57dc4ae1c42751eec
Cris Mihalache <me@f3rno.com>
2020-03-12 17:23:29 +0700

    (WIP)

--- README.md
+++ README.md
@@ -1,3 +1,60 @@
 # Bitfinex HF Functional Strategy Module
 
-TODO
+> Featherweight successor to `bfx-hf-strategy`
+
+A light strategy framework utilizing a single function for execution, with support for backtesting and operating on multiple markets/symbols simultaneously. Heavily WIP, full description pending.
+
+See JSDoc-generated documentation in `docs/`.
+
+### Example
+
+```js
+'use strict'
+
+process.env.DEBUG = 'bfx:hf:*'
+
+const debug = require('debug')('bfx:hf:func:examples:ema-cross')
+const data = require('bfx-hf-func-data')
+const { EMA } = require('bfx-hf-indicators')
+const HFF = require('../')
+
+const emasCrossed = ({ s, l }) => s.crossed(l.v())
+
+const strategy = async (f, data) => {
+
+  await f.defineIndicators({
+    l: new EMA([100]),
+    s: new EMA([20])
+  })
+
+  return f.condition.indicators(emasCrossed, async ({ s, l } = {}) => {
+    const price = HFF.priceForData(data)
+
+    debug('indicators crossed: s:%f <-> l:%f', s.v(), l.v())
+
+    if (f.inPosition()) {
+      return f.closePosition({ price })
+    } else {
+      return f.openPosition({ amount: 6, price })
+    }
+  })
+}
+
+const run = async () => {
+  await HFF.backtest({
+    strategy,
+    defaultSymbol: 'tLEOUSD',
+    datasets: {
+      tLEOUSD: data['trade:1m:tLEOUSD']
+    }
+  })
+
+  HFF.logResults(strategy)
+}
+
+try {
+  run()
+} catch (e) {
+  debug('error: %s', e.stack)
+}
+```
--- examples/ema_cross.js
+++ examples/ema_cross.js
@@ -5,26 +5,22 @@ process.env.DEBUG = 'bfx:hf:*'
 const debug = require('debug')('bfx:hf:func:examples:ema-cross')
 const data = require('bfx-hf-func-data')
 const { EMA } = require('bfx-hf-indicators')
-const _reduce = require('lodash/reduce')
 const HFF = require('../')
 
 const emasCrossed = ({ s, l }) => s.crossed(l.v())
 
 const strategy = async (f, data) => {
-  const price = HFF.priceForData(data)
-
   await f.defineIndicators({
     l: new EMA([100]),
     s: new EMA([20])
   })
 
-  // await f.breakIf.indicators(({ s, l }) => s.crossed(l.v()))
-
   return f.condition.indicators(emasCrossed, async ({ s, l } = {}) => {
+    const price = HFF.priceForData(data)
+
     debug('indicators crossed: s:%f <-> l:%f', s.v(), l.v())
 
     if (f.inPosition()) {
-      console.log({ openPrice: price })
       return f.closePosition({ price })
     } else {
       return f.openPosition({ amount: 6, price })
@@ -39,18 +35,6 @@ const strategy = async (f, data) => {
   */
 }
 
-// TODO: extract as util
-const debugResults = (strategy) => {
-  const trades = HFF.getState(strategy).get('trades').toJS()
-  const positions = HFF.getState(strategy).get('positions').toJS()
-  const pl = _reduce(trades.map(t => t.nv), (prev, acc) => prev + acc, 0)
-
-  console.log(pl)
-  console.log('open positions:')
-  console.log(positions)
-  console.log(positions.tLEOUSD.trades)
-}
-
 const run = async () => {
   await HFF.backtest({
     strategy,
@@ -60,7 +44,7 @@ const run = async () => {
     }
   })
 
-  debugResults(strategy)
+  HFF.logResults(strategy)
 }
 
 try {
